165b5691b9e1b8b9e2935baedb60d599717f8690
maigithome <mai7@bk.ru>
2022-08-08 12:00:46 +0500

    Zadacha 13

--- Zadacha13/Program.cs
+++ Zadacha13/Program.cs
@@ -4,11 +4,14 @@
 // 32679 -> 6
 
 Console.Write("Введите число: ");
-string inputNumber = Convert.ToInt32(Console.ReadLine());
+string inputNumber = Console.ReadLine();
 
-double digit  = (inputNumber / 100);
+var number = int.Parse(inputNumber);
+
+double digit = number/100;
 
 if (digit < 1) Console.WriteLine("Третьей цифры нет");
 
 else Console.WriteLine($"Третья цифра числа {inputNumber[2]}");
+
 
