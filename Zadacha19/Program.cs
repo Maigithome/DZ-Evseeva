7bc0760c7b53ea0390ec320a436dc1e823e024bf
maigithome <mai7@bk.ru>
2022-08-13 18:51:54 +0500

    Zadacha19

--- Zadacha19/Program.cs
+++ Zadacha19/Program.cs
@@ -6,18 +6,26 @@
 
 
 
-Console.WriteLine("Введите число:");
-string str = Console.ReadLine();
-int prov = 0;
-for (int i = 0; i < str.Length / 2; i++)
-{
-    if (str.Substring(i, 1) != (str.Substring(str.Length - 1 - i, 1)))
-    {
-        Console.WriteLine("Число {0} не является палиндромом", str);
-     break;
-    }
-    else
-     prov = 1;
-}
-if (prov == 1) Console.WriteLine("Число {0} является палиндромом", str);
+// Console.WriteLine("Введите число:");
+// string str = Console.ReadLine();
+// int prov = 0;
+// for (int i = 0; i < str.Length / 2; i++)
+// {
+//     if (str.Substring(i, 1) != (str.Substring(str.Length - 1 - i, 1)))
+//     {
+//         Console.WriteLine("Число {0} не является палиндромом", str);
+//      break;
+//     }
+//     else
+//      prov = 1;
+// }
+// if (prov == 1) Console.WriteLine("Число {0} является палиндромом", str);
 
+Console.Write("Введите число: ");
+string inputNumber = Console.ReadLine();
+
+var number = int.Parse(inputNumber);
+
+if (inputNumber[0]==inputNumber[4]&&inputNumber[1]==inputNumber[3]) Console.WriteLine("Да, число является палиндромом");
+
+else Console.WriteLine($"Нет, число не является палиндромом");
