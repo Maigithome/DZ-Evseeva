b4435031fcf6561b7aac998c9d29c1c9c0b80af5
maigithome <mai7@bk.ru>
2022-08-21 21:10:32 +0500

    Zadacha41

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// Console.WriteLine("Введите числа: ");
+// int[] array = new int [5];
+
+
+int[] array = { 0, 7, 8, -2, -2 };
+int j = 0;
+int arraySize = array.Length;
+int count = 0;
+Console.WriteLine($"Array: [ {String.Join("; ", array)} ]");
+for (j = 0; j < arraySize; j++)
+{
+    if (array[j] > 0) count++;
+}
+Console.WriteLine($"Чисел больше нуля - {count}");
