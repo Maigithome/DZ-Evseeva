5b2e86677e3bf1cfca7dfc65d44c16d616ac2f9e
maigithome <mai7@bk.ru>
2022-08-29 21:52:00 +0500

    Zadacha60

---
+++ Zadacha60/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// Задача 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+
+Console.Write("Введите x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+int[,,] array = new int[x, y, z];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+            array[i, j, k] = Convert.ToInt32(new Random().Next(10, 100));
+    }
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+            Console.Write($"{array[i, j, k]} {(i, j, k)}; ");
+            Console.WriteLine(); 
+    }
+   Console.WriteLine(); 
+}
